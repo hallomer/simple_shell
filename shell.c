47ddeb203151d8aa140d6d6ffa5b9cb4db1f502d
hallomer <hebaaltayeb2@icloud.com>
2024-01-14 14:13:53 -0800

    please

--- shell.c
+++ shell.c
@@ -1,158 +1,104 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * main - Entry point for the shell
+ * display_prompt - Displays the prompt
  *
- * Return: 0 on sucsess
+ * Return: the command entered by the user
 */
-
-int main(void)
+void display_prompt(void)
 {
-	char *cmd, *new_line = NULL, *token;
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+}
+
+/**
+ * read_cmd - Reads a command from the user
+ *
+ * Return: the command entered by the user
+*/
+char *read_cmd(void)
+{
+	char *cmd = NULL;
 	size_t cmd_size = 0;
 	ssize_t read_cmd;
-	int status = 0;
 
-	while (1)
+	read_cmd = getline(&cmd, &cmd_size, stdin);
+	if (read_cmd == -1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 2);
-
-		read_cmd = getline(&cmd, &cmd_size, stdin);
-		if (read_cmd == -1 || is_empty(cmd))
-		{
-			free(cmd);
-			exit(status);
-		}
-		is_newline(cmd);
-		if (is_env(cmd) != 1)
-		{
-			new_line = malloc(strlen(cmd) + 1);
-			strcpy(new_line, cmd);
-			if (is_empty(new_line))
-			{
-				free(cmd);
-				free(new_line);
-				exit(status);
-			}
-
-			token = strtok(new_line, " \t\n");
-			while (token)
-				token = strtok(NULL, " \t\n");
-
-			free(new_line);
-		}
-		 execute(cmd);
-	}
-	return (0);
-}
-
-
-/**
- * is_empty - Check if string is empty or space
- * @str: string
- *
- * Return: 1 if not empty
-*/
-
-int is_empty(const char *str)
-{
-	if (str == NULL)
-		return (1);
-
-	while (*str)
-	{
-		if (*str != ' ' && *str != '\t' && *str != '\n' &&
-			*str != '\v' && *str != '\f' && *str != '\r')
-			return (0);
-		str++;
+		free(cmd);
+		exit(EXIT_SUCCESS);
 	}
 
-	return (1);
-}
+	cmd[strcspn(cmd, "\n")] = '\0';
+
+	return (cmd);
+	}
+
 /**
- * is_newline - If there is \n char, it removes it
+ * execute_cmd - Executes a command with the given arguments
  * @cmd: command
  *
- * Return: nothing
+ * Return: 0 on success
 */
-void is_newline(char *cmd)
+void execute_cmd(char *cmd)
 {
-	int i = 0;
-
-	while (cmd[i] != '\0')
-	{
-		if (cmd[i] == '\n')
-			cmd[i] = '\0';
-		i++;
-	}
-}
-
-
-
-/**
- * is_env - Check if command is env
- * @cmd: command
- *
- * Return: 1 if env
-*/
-int is_env(char *cmd)
-{
-	int i = 0;
-
-	if (strncmp(cmd, "env", 3) == 0)
-	{
-		for (i = 0; environ[i]; i++)
-		{
-			write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-			write(STDOUT_FILENO, "\n", 1);
-		}
-		return (1);
-	}
-	return (0);
-}
-
-
-/**
- * execute - Executes commands
- * @cmd: command
- *
- * Return: nothing
-*/
-void execute(char *cmd)
-{
-	char *args[1024], *token = strtok(cmd, " \t\n");
-	int i = 0;
 	pid_t pid;
+	char *envp[] = {NULL};
 
-	while (token != NULL)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " \t\n");
-	}
-	args[i] = NULL;
 	pid = fork();
-
 	if (pid == -1)
 	{
-
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
+		char **args = malloc(sizeof(char *) * 2);
 
-		if (execvp(args[0], args) == -1)
+		args[0] = cmd;
+		args[1] = NULL;
+
+		if (execve(cmd, args, envp) == -1)
 		{
-			perror("execvp");
+			perror(cmd);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
-		int status;
+		int child_status;
 
-		waitpid(pid, &status, 0);
+		if (waitpid(pid, &child_status, 0) == -1)
+		{
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
 	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing
+*/
+int main(void)
+{
+	char *cmd;
+
+	while (1)
+	{
+		display_prompt();
+		cmd = read_cmd();
+		execute_cmd(cmd);
+		free(cmd);
+	}
+
+	return (0);
 }
